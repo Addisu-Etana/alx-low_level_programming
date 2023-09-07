1fcbdacdd4d38c85ed18dbd35d1c1ba8d2338b96
Addisu-Etana <addisuelectrical@gmail.com>
2023-09-07 21:10:43 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
-/* main entry programming puzzl*/
+/**
+  *  main entry programming puzzl
+  *  Return: 0
+  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
-
