11bfe431054b05e5cba83e0394ace9d672987903
Addisu-Etana <addisuelectrical@gmail.com>
2023-09-08 14:31:05 -0700

    6-prints_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - main function single digit numbers of base 10 function
+  * return: eachtime 0
+  */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		putchar(a + '0');
+	putchar('\n');
+	return (0);
+}
