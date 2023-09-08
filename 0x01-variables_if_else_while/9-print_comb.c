5b6657134b95cfdbd52bf67e5e606f2d30c3dc66
Addisu-Etana <addisuelectrical@gmail.com>
2023-09-08 15:57:13 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,15 @@ int main(void)
 	int i;
 
 	for (i = 0; i < 10; i++)
+{
+	if (i == 9)
+		putchar(i + '0');
+	else
 	{
 		putchar(i + '0');
 		putchar(',');
 		putchar(' ');
 	}
-	return (0);
 }
-
+return (0);
+}
