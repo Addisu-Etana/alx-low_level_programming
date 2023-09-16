49a124c418c77231a6576093e947f9505e0440dd
Addisu-Etana <addisuelectrical@gmail.com>
2023-09-16 08:31:36 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * mul -function checks multiple of two digit
+  * @a: a- var
+  * @b: b- var
+  * Return: eachtime 0
+  */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
+
