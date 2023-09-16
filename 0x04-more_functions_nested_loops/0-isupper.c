eeca5578be33a7341d5682e660e3ebd85a684890
Addisu-Etana <addisuelectrical@gmail.com>
2023-09-16 07:34:54 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
-  * isupper programming check
-  *Return: eachtime 0
+  * _isupper -prog checks for uppercase character
+  * @c: variable text
+  * Return: Always 0.
   */
 int _isupper(int c)
 {
-	if(c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 }
