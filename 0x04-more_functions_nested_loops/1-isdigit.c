fd8a8aa2e2bfdf7de707a9acd9d55a98ddf3c5f7
Addisu-Etana <addisuelectrical@gmail.com>
2023-09-16 08:09:17 -0700

    1-isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *_isdigit - function for check
+  *
+  *int c: return value
+  *
+  *c: for check digit
+  *
+  *Return: 1 if c is digit, 0 others
+  */
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
+
