2498730579a69d0a0bed3bfecf40634af47706a4
Addisu-Etana <addisuelectrical@gmail.com>
2023-09-18 14:43:08 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+  * puts2 - prints every other character of a string
+  *
+  *@str: input
+  * Return: always 0
+  */
+void puts2(char *str)
+{
+	int a = 0;
+	int b = 0;
+	char *y = str;
+	int x;
+
+	while (*y != '\0')
+	{
+		y++;
+		a++;
+	}
+	b = a - 1;
+	for (x = 0; x < = b; x++)
+	{
+		if (x % 2 == 0)
+		{
+			_putchar(str[x]);
+		}
+	}
+	_putchar('\n');
+}
