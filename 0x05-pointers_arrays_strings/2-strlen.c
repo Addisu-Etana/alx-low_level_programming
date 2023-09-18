01365878d47d57819f3fc8b31129c1f5e5e6ad08
Addisu-Etana <addisuelectrical@gmail.com>
2023-09-18 13:13:09 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+   #include "main.h"
+   /**
+   *_strlen- return the length of a string
+   *
+   *@s: string of character
+   *Return: length
+   */
+	int _strlen(char *s)
+{
+	int a = 0;
+
+	while (*s != '\n')
+		a++;
+		s++;
+}
+return (a);
+}
