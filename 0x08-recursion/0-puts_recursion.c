33efcd3bf9438918e90e52d9613f67d4ad4e3f22
Addisu-Etana <addisuelectrical@gmail.com>
2023-09-29 00:55:12 -0700

    0-puts-recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  *
+  * _puts_recursion - recursive function to print character
+  * @s: input
+  * Returns: eachtime 0
+  */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar("\n");
+}
