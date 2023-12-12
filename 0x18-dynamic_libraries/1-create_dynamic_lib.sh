f0be36ff4765304b79d0ee3ebc5f5bc95bee562d
Addisu-Etana <addisuelectrical@gmail.com>
2023-12-12 03:00:09 -0800

    task 2

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-gcc -c -fPIC *.c
-gcc -shared -o liball.so *.o 
+gcc gcc -Wall -pedantic -Werror -pedantic -c -fpic *.c
+gcc -shared -o liball.so *.o
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
